b9ffda222e75a080f74b86d09b0d97896de5d4fb
Geert Bevin <gbevin@uwyn.com>
2023-03-25 09:17:04 -0400

    bld 1.5.6

--- Formula/bld.rb
+++ Formula/bld.rb
@@ -1,8 +1,8 @@
 class Bld < Formula
   desc "Open-source build tool for the RIFE2 Java web framework"
   homepage "https://rife2.com"
-  url "https://repo1.maven.org/maven2/com/uwyn/rife2/rife2/1.5.5/rife2-1.5.5-bld.zip"
-  sha256 "a65a5618fec2c52034dd2a045d8518709efe5833ce040e379590f94a3bf50881"
+  url "https://repo1.maven.org/maven2/com/uwyn/rife2/rife2/1.5.6/rife2-1.5.6-bld.zip"
+  sha256 "32ae08bb3b891f7c059afb05d2a8bb3907ce1da1f5f6cf7ff3f4b0598f9995a2"
   license "Apache-2.0"
 
   depends_on "openjdk"
@@ -10,7 +10,7 @@ class Bld < Formula
   def install
     rm_f Dir["bin/*.bat"]
     libexec.install %w[bin lib]
-    env = Language::Java.overridable_java_home_env("19")
+    env = Language::Java.overridable_java_home_env("17")
     (bin/"bld").write_env_script libexec/"bin/bld", env
   end
 
