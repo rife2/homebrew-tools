e3cc375b9a337176625c978f0b2874f5e74c4dd7
Geert Bevin <gbevin@uwyn.com>
2023-05-01 16:00:41 -0400

    bld 1.6.2

--- Formula/bld.rb
+++ Formula/bld.rb
@@ -1,8 +1,8 @@
 class Bld < Formula
   desc "Open-source build tool for the RIFE2 Java web framework"
   homepage "https://rife2.com"
-  url "https://repo.rife2.com/releases/com/uwyn/rife2/rife2/1.6.1/rife2-1.6.1-bld.zip"
-  sha256 "2ce8b1b5db3798c20fcf5c822da96a7ecb573f1e899b259262299b34b6e49259"
+  url "https://repo.rife2.com/releases/com/uwyn/rife2/rife2/1.6.2/rife2-1.6.2-bld.zip"
+  sha256 "4a5c0ab66d6feceaa692a71b20573bf585f43a742739feaf42f1b4fff072a391"
   license "Apache-2.0"
 
   depends_on "openjdk"
