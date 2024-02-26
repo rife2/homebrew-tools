2c7bd8369544e7ada0b5726590bea0678049cedb
Geert Bevin <gbevin@uwyn.com>
2024-02-25 23:01:54 -0500

    bld 1.9.0

--- Formula/bld.rb
+++ Formula/bld.rb
@@ -1,8 +1,8 @@
 class Bld < Formula
   desc "Pure java build tool for developers who don't like dealing with build tools"
   homepage "https://rife2.com/bld"
-  url "https://repo1.maven.org/maven2/com/uwyn/rife2/bld/1.8.0/bld-1.8.0.zip"
-  sha256 "f6671a28cb26aac01a98a1df44ef48b9110110a5d689150f8cdb523d694b1b24"
+  url "https://repo1.maven.org/maven2/com/uwyn/rife2/bld/1.9.0/bld-1.9.0.zip"
+  sha256 "d9b18bae68c46439a9ad2e86430914fd942b9b89e6771d78d6289dc0cb6fc844"
   license "Apache-2.0"
 
   depends_on "openjdk"
