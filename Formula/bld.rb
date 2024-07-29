ce48b32a725ec4103cd620da1d74e3fcf6627dc7
Geert Bevin <gbevin@uwyn.com>
2024-07-28 20:21:21 -0400

    bld 2.0.0 sha256 fix

--- Formula/bld.rb
+++ Formula/bld.rb
@@ -2,7 +2,7 @@ class Bld < Formula
   desc "Pure java build tool for developers who don't like dealing with build tools"
   homepage "https://rife2.com/bld"
   url "https://repo1.maven.org/maven2/com/uwyn/rife2/bld/2.0.0/bld-2.0.0.zip"
-  sha256 "2de7f26f0f028634a2ec22436617917c0176698754cb54886bcd49f35e8b6b86"
+  sha256 "0e1a0f3c8416c790288744e2a28419d6ace42f119889c4c3a1335053ab32e409"
   license "Apache-2.0"
 
   depends_on "openjdk"
